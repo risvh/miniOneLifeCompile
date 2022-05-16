166a02da9aeac9e138c6b286d63c454d484d074e
risvh <67486979+risvh@users.noreply.github.com>
2022-05-16 23:03:14 +0800

    Not clearing cache by default, clean .dep2 files too

--- cleanOldBuildsAndCaches.sh
+++ cleanOldBuildsAndCaches.sh
@@ -1,7 +1,10 @@
 set -e
 cd "$(dirname "${0}")/.."
 
+clearCache=${1-0}
+
 find . -type f -name '*.o' -exec rm {} +
 find . -type f -name '*.dep' -exec rm {} +
+find . -type f -name '*.dep2' -exec rm {} +
 
-find . -type f -name '*.fcz' -exec rm {} +
+if [[ $clearCache == 1 ]]; then find . -type f -name '*.fcz' -exec rm {} +; fi
