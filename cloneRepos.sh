67a7d0a69cb6c4b8ee5c7e1bfdc4932b8b43c362
Ira Casper <Ira.Casper@gmail.com>
2022-05-25 17:41:40 -0700

    Adjust cloneRepos to have access to all repos at once, via remotes.

--- cloneRepos.sh
+++ cloneRepos.sh
@@ -2,29 +2,51 @@
 cd "$(dirname "${0}")/.."
 
 #2HOL
-CODE_REPO="https://github.com/twohoursonelife/OneLife.git"
-DATA_REPO="https://github.com/twohoursonelife/OneLifeData7.git"
-GEMS_REPO="https://github.com/twohoursonelife/minorGems.git"
+THOL_CODE_REPO="https://github.com/twohoursonelife/OneLife.git"
+THOL_DATA_REPO="https://github.com/twohoursonelife/OneLifeData7.git"
+THOL_GEMS_REPO="https://github.com/twohoursonelife/minorGems.git"
 
 #Hetuw for 2HOL with minitech
-# CODE_REPO="https://github.com/risvh/OneLife-1.git"
-# DATA_REPO="https://github.com/twohoursonelife/OneLifeData7.git"
-# GEMS_REPO="https://github.com/hetuw/minorGems.git"
+RISVH_CODE_REPO="https://github.com/risvh/OneLife-1.git"
+# THOL_DATA_REPO="https://github.com/twohoursonelife/OneLifeData7.git"
+HETUW_GEMS_REPO="https://github.com/hetuw/minorGems.git"
 
 #Hetuw v268 (for 2HOL, before login format was changed)
-# CODE_REPO="https://github.com/hetuw/OneLife.git"
-# DATA_REPO="https://github.com/twohoursonelife/OneLifeData7.git"
-# GEMS_REPO="https://github.com/hetuw/minorGems.git"
+HETUW_CODE_REPO="https://github.com/hetuw/OneLife.git"
+# THOL_DATA_REPO="https://github.com/twohoursonelife/OneLifeData7.git"
+# HETUW_GEMS_REPO="https://github.com/hetuw/minorGems.git"
 
 #OHOL
-# CODE_REPO="https://github.com/jasonrohrer/OneLife.git"
-# DATA_REPO="https://github.com/jasonrohrer/OneLifeData7.git"
-# GEMS_REPO="https://github.com/jasonrohrer/minorGems.git"
+OHOL_CODE_REPO="https://github.com/jasonrohrer/OneLife.git"
+OHOL_DATA_REPO="https://github.com/jasonrohrer/OneLifeData7.git"
+OHOL_GEMS_REPO="https://github.com/jasonrohrer/minorGems.git"
 
 
-if [[ ! -d OneLife ]]; then git clone $CODE_REPO OneLife; fi
-if [[ ! -d OneLifeData7 ]]; then git clone $DATA_REPO OneLifeData7; fi
-if [[ ! -d minorGems ]]; then git clone $GEMS_REPO minorGems; fi
+MY_CODE_REPO="$RISVH_CODE_REPO"
+MY_DATA_REPO="$THOL_DATA_REPO"
+MY_GEMS_REPO="$HETUW_GEMS_REPO"
+if [[ ! -d OneLife ]]; then
+	git clone $MY_CODE_REPO OneLife
+	git -C OneLife remote add hetuw $HETUW_CODE_REPO
+	git -C OneLife remote add risvh $RISVH_CODE_REPO
+	git -C OneLife remote add jasonrohrer $OHOL_CODE_REPO
+	git -C OneLife remote add twohoursonelife $THOL_CODE_REPO
+fi
+if [[ ! -d OneLifeData7 ]]; then
+	git clone $MY_DATA_REPO OneLifeData7
+	git -C OneLifeData7 remote add twohoursonelife $THOL_DATA_REPO
+	git -C OneLifeData7 remote add jasonrohrer $OHOL_DATA_REPO
+fi
+if [[ ! -d minorGems ]]; then
+	git clone $MY_GEMS_REPO minorGems
+	git -C minorGems remote add twohoursonelife $THOL_GEMS_REPO
+	git -C minorGems remote add jasonrohrer $OHOL_GEMS_REPO
+	git -C minorGems remote add hetuw $HETUW_GEMS_REPO
+fi
+
+git -C OneLife fetch --all
+git -C OneLifeData7 fetch --all
+git -C minorGems fetch --all
 
 #Hetuw for 2HOL with minitech (Town planner)
 # git -C "OneLife" checkout townPlanner;
