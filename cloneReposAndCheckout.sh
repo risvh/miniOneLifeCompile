14f9f1b0dc77d805f404d52de7c07652f50883ac
Ira Casper <Ira.E.Casper@gmail.com>
2022-05-28 12:51:47 -0700

    Add thol as an additional remote, so cloneRepos doesn't require origin

--- cloneReposAndCheckout.sh
+++ cloneReposAndCheckout.sh
@@ -19,17 +19,20 @@ if [[ ! -d OneLife ]]; then
 	git -C OneLife remote add ohol_hetuw $HETUW_CODE_REPO
 	git -C OneLife remote add thol_hetuw $RISVH_CODE_REPO
 	git -C OneLife remote add ohol $OHOL_CODE_REPO
+	git -C OneLife remote add thol $THOL_CODE_REPO
 	git -C OneLife fetch --all
 fi
 if [[ ! -d OneLifeData7 ]]; then
 	git clone $THOL_DATA_REPO OneLifeData7
 	git -C OneLifeData7 remote add ohol $OHOL_DATA_REPO
+	git -C OneLifeData7 remote add thol $THOL_CODE_REPO
 	git -C OneLifeData7 fetch --all
 fi
 if [[ ! -d minorGems ]]; then
 	git clone $THOL_GEMS_REPO minorGems
 	git -C minorGems remote add ohol_hetuw $HETUW_GEMS_REPO
 	git -C minorGems remote add ohol $OHOL_GEMS_REPO
+	git -C minorGems remote add thol $THOL_CODE_REPO
 	git -C minorGems fetch --all
 fi
 
@@ -50,12 +53,12 @@ if [[ "$1" == "" ]]; then
 fi
 
 if [[ "$repo_option" == "thol" ]]; then
-	git -C OneLife checkout origin/master
-	git -C OneLifeData7 checkout origin/master
-	git -C minorGems checkout origin/master
+	git -C OneLife checkout thol/master
+	git -C OneLifeData7 checkout thol/master
+	git -C minorGems checkout thol/master
 elif [[ "$repo_option" == "thol_hetuw" ]]; then
 	git -C OneLife checkout thol_hetuw/master
-	git -C OneLifeData7 checkout origin/master
+	git -C OneLifeData7 checkout thol/master
 	git -C minorGems checkout ohol_hetuw/master
 	# Hetuw v268 works for 2HOL, that was before login format changes
 	# git -C OneLife checkout 1ab17ba #268 Oct4 2019
@@ -64,7 +67,7 @@ elif [[ "$repo_option" == "townplanner" ]]; then
 	# Town planner is on a branch of the thol_hetuw repo
 	# it uses an old version of ohol_hetuw minorGems
 	git -C OneLife checkout thol_hetuw/townPlanner
-	git -C OneLifeData7 checkout origin/master
+	git -C OneLifeData7 checkout thol/master
 	git -C minorGems checkout 4f3991f #266 Sep28 2019
 elif [[ "$repo_option" == "ohol" ]]; then
 	git -C OneLife checkout ohol/master
