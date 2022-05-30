40148a3e26212495eee53acc4d11772a36b89c36
risvh <67486979+risvh@users.noreply.github.com>
2022-05-30 22:50:11 +0800

    oops typos

--- cloneReposAndCheckout.sh
+++ cloneReposAndCheckout.sh
@@ -27,14 +27,14 @@ fi
 if [[ ! -d OneLifeData7 ]]; then
 	git clone $THOL_DATA_REPO OneLifeData7
 	git -C OneLifeData7 remote add ohol $OHOL_DATA_REPO
-	git -C OneLifeData7 remote add thol $THOL_CODE_REPO
+	git -C OneLifeData7 remote add thol $THOL_DATA_REPO
 	git -C OneLifeData7 fetch --all
 fi
 if [[ ! -d minorGems ]]; then
 	git clone $THOL_GEMS_REPO minorGems
 	git -C minorGems remote add ohol_hetuw $HETUW_GEMS_REPO
 	git -C minorGems remote add ohol $OHOL_GEMS_REPO
-	git -C minorGems remote add thol $THOL_CODE_REPO
+	git -C minorGems remote add thol $THOL_GEMS_REPO
 	git -C minorGems fetch --all
 fi
 
