6ab65dcf44e25dbb8f540b0d088e3d212f1769d0
risvh <67486979+risvh@users.noreply.github.com>
2022-08-01 00:01:30 +0800

    Fixed broken path after moving scripts around

--- repo/testPullRequest.sh
+++ repo/testPullRequest.sh
@@ -7,7 +7,7 @@ remote=$2
 prNum=$3
 
 ### Clean repo first
-./miniOneLifeCompile/ensureClean.sh $1
+./miniOneLifeCompile/repo/ensureClean.sh $1
 
 ### Fetch and checkout the PR as a new local branch
 git -C $repo fetch $remote pull/$prNum/head:${remote}_pr_$prNum
