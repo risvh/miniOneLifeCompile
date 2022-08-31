c2b712447f56468391f5836dae7fc11c4e5f5fd0
risvh <67486979+risvh@users.noreply.github.com>
2022-08-31 21:03:16 +0800

    Update makeFullGameFolder.sh
    make sure this works tho not used often

--- makeFullGameFolder.sh
+++ makeFullGameFolder.sh
@@ -20,7 +20,7 @@ for f in $folderList; do
 	# if [[ $PLATFORM == 1 ]] && [ ! -h $f ]; then ln -s ../OneLifeData7/$f .; fi
     # if [[ $PLATFORM == 5 ]] && [ ! -h $f ]; then ../miniOneLifeCompile/mklink.sh /J $f ../OneLifeData7/$f; fi
 	
-	###### Full copy from OneLifeData7 repo
+	###### Full copy
 	# if [ ! -d $f ]; then cp -RL -v ../OneLifeData7/$f .; fi
 	
 	###### Sync only when different
@@ -28,6 +28,28 @@ for f in $folderList; do
     mkdir -p $f
     cd $f
     rsync -vr ../../OneLifeData7/$f/ .
+    cd ..
+	
+done;
+
+
+folderList="graphics otherSounds languages"
+
+#Copying from OneLife/gameSource
+for f in $folderList; do
+    
+	###### Create sym link only
+	# if [[ $PLATFORM == 1 ]] && [ ! -h $f ]; then ln -s ../OneLife/gameSource/$f .; fi
+    # if [[ $PLATFORM == 5 ]] && [ ! -h $f ]; then ../miniOneLifeCompile/mklink.sh /J $f ../OneLife/gameSource/$f; fi
+	
+	###### Full copy
+	# if [ ! -d $f ]; then cp -RL -v ../OneLife/gameSource/$f .; fi
+	
+	###### Sync only when different
+    if [ -h $f ]; then rm $f; fi
+    mkdir -p $f
+    cd $f
+    rsync -vr ../../OneLife/gameSource/$f/ .
     cd ..
 	
 done;
