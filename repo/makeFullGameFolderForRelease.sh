8c67fe4ee8e6b46f666f03b871d568d4826fd7c2
risvh <67486979+risvh@users.noreply.github.com>
2022-09-27 20:46:34 +0800

    Remade this script for release use
    Full release folder for diffBundle to create the patches for autoupdate

---
+++ repo/makeFullGameFolderForRelease.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+set -e
+PLATFORM=$(cat PLATFORM_OVERRIDE)
+if [[ $PLATFORM != 1 ]] && [[ $PLATFORM != 5 ]]; then PLATFORM=${1-5}; fi
+if [[ $PLATFORM != 1 ]] && [[ $PLATFORM != 5 ]]; then
+	echo "Usage: 1 for Linux, 5 for XCompiling for Windows (Default)"
+	exit 1
+fi
+cd "$(dirname "${0}")/.."
+
+
+###### Name the release folder
+
+version=$(<OneLifeData7/dataVersionNumber.txt)
+if [[ $PLATFORM == 1 ]]; then outputName="2HOL_v${version}_linux"; fi
+if [[ $PLATFORM == 5 ]]; then outputName="2HOL_v${version}_win"; fi
+
+mkdir -p $outputName
+cd $outputName
+
+
+###### Asset folders and other files
+
+pathList=()
+folderList=()
+
+temp="animations categories ground music objects sounds sprites transitions"
+for f in $temp; do folderList+=( "$f" ); done;
+for f in $temp; do pathList+=( "../OneLifeData7/$f" ); done;
+
+temp="graphics otherSounds languages settings"
+for f in $temp; do folderList+=( "$f" ); done;
+for f in $temp; do pathList+=( "../OneLife/gameSource/$f" ); done;
+
+
+for i in ${!pathList[@]}; do
+    
+	###### Create sym link only
+	# if [[ $PLATFORM == 1 ]] && [ ! -h ${folderList[$i]} ]; then ln -s ${pathList[$i]} .; fi
+    # if [[ $PLATFORM == 5 ]]; then
+        # if [ -h ${folderList[$i]} ]; then rm ./${folderList[$i]}; fi
+        # ../miniOneLifeCompile/util/mklink.sh /J ${folderList[$i]} ${pathList[$i]}
+    # fi
+	
+	###### Full copy
+    if [ -e ${folderList[$i]} ]; then rm ./${folderList[$i]}; fi
+	cp -RL -v ${pathList[$i]} .
+	
+	###### Sync only when different
+    # if [ -h ${folderList[$i]} ]; then rm ${folderList[$i]}; fi
+    # mkdir -p ${folderList[$i]}
+    # rsync -vru --delete ${pathList[$i]}/ ${folderList[$i]}
+	
+done;
+
+
+cp ../OneLife/gameSource/language.txt .
+cp ../OneLife/gameSource/us_english_60.txt .
+cp ../OneLife/documentation/Readme.txt .
+cp ../OneLife/no_copyright.txt .
+cp ../OneLife/gameSource/reverbImpulseResponse.aiff .
+cp ../OneLife/server/wordList.txt .
+
+d=$(TZ=":GMT" date)
+echo "$1 built on $d" > binary.txt
+
+
+###### Data version
+
+cp ../OneLifeData7/dataVersionNumber.txt .
+
+
+###### SDL
+
+if [[ $PLATFORM == 5 ]] && [ ! -f SDL.dll ]; then cp ../OneLife/build/win32/SDL.dll .; fi
+
+
+###### EOL
+
+if [[ $PLATFORM == 1 ]]; then
+    find . -type f -name '*.txt' -exec sed -i 's/\r//g' {} +
+fi
+
+if [[ $PLATFORM == 5 ]]; then
+    cp ../OneLife/build/unix2dos.c .
+    cp ../OneLife/build/unix2dosScript .
+    g++ -o unix2dos unix2dos.c
+
+    find . -type f -name 'unix2dosScript' -exec sed -i 's/\r//g' {} +
+
+    for file in $( find . -name "*.txt" )
+    do
+        echo "$file"
+        ./unix2dosScript "$file"
+    done
+
+    rm unix2dos.c
+    rm unix2dosScript
+    rm unix2dos
+fi
+
+
+###### Copy the game exe
+
+if [[ $PLATFORM == 1 ]]; then
+    cp ../output/OneLife ./OneLifeApp
+fi
+
+if [[ $PLATFORM == 5 ]]; then
+    cp ../output/OneLife.exe .
+fi
