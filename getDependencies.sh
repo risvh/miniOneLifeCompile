fbbf07c31877c200d3eacc23d5937642f57d15bb
risvh <67486979+risvh@users.noreply.github.com>
2022-11-23 19:17:33 +0800

    Combined the scripts for dependencies
    as suggested by @Riik, since libpng is required by the game client now

--- getDependencies.sh
+++ getDependencies.sh
@@ -18,6 +18,7 @@ sudo apt-get install -y build-essential
 mkdir -p dependencies
 cd dependencies
 
+# Getting SDL
 if [ ! -d SDL* ]; then
 	wget https://www.libsdl.org/release/SDL-1.2.15.tar.gz -O- | tar xfz -
 	cd SDL*
@@ -25,6 +26,36 @@ if [ ! -d SDL* ]; then
 		--bindir=/usr/i686-w64-mingw32/bin \
 		--libdir=/usr/i686-w64-mingw32/lib \
 		--includedir=/usr/i686-w64-mingw32/include \
+		--host=i686-w64-mingw32 \
+		--prefix=/usr/i686-w64-mingw32 \
+		CPPFLAGS="-I/usr/i686-w64-mingw32/include" \
+		LDFLAGS="-L/usr/i686-w64-mingw32/lib"
+	make
+	sudo make install
+fi
+
+# Getting zlib
+if [ ! -d zlib* ]; then
+	wget http://zlib.net/fossils/zlib-1.2.12.tar.gz -O- | tar xfz -
+	cd zlib*
+	host="i686-w64-mingw32"
+	prefixdir="/usr/i686-w64-mingw32"
+	sudo make -f win32/Makefile.gcc \
+		BINARY_PATH=$prefixdir/bin \
+		INCLUDE_PATH=$prefixdir/include \
+		LIBRARY_PATH=$prefixdir/lib \
+		SHARED_MODE=1 \
+		PREFIX=$host- \
+		install
+fi
+
+cd ..
+
+# Getting libpng
+if [ ! -d l*png* ]; then
+	wget http://downloads.sourceforge.net/project/libpng/libpng16/1.6.37/libpng-1.6.37.tar.gz -O- | tar xfz -
+	cd l*png*
+	./configure \
 		--host=i686-w64-mingw32 \
 		--prefix=/usr/i686-w64-mingw32 \
 		CPPFLAGS="-I/usr/i686-w64-mingw32/include" \
