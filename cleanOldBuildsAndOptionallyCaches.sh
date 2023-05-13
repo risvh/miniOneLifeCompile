7b6c0a14c09fd616076140e5f27ba813182d1036
Eboubaker Bekkouche <eboubakkar@gmail.com>
2023-05-13 21:27:55 +0100

    cleanOldBuildsAndOptionallyCaches.sh rm command is requesting confirmation for each file

--- cleanOldBuildsAndOptionallyCaches.sh
+++ cleanOldBuildsAndOptionallyCaches.sh
@@ -4,8 +4,8 @@ cd "$(dirname "${0}")/.."
 
 clearCache=${1-0}
 
-find . -type f -name '*.o' -exec rm {} +
-find . -type f -name '*.dep' -exec rm {} +
-find . -type f -name '*.dep2' -exec rm {} +
+find . -type f -name '*.o' -exec rm -f {} +
+find . -type f -name '*.dep' -exec rm -f {} +
+find . -type f -name '*.dep2' -exec rm -f {} +
 
-if [[ $clearCache == 1 ]]; then find . -type f -name '*.fcz' -exec rm {} +; fi
+if [[ $clearCache == 1 ]]; then find . -type f -name '*.fcz' -exec rm -f {} +; fi
