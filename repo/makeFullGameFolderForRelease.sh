b92fb84ba587e17c3e5758ceb61164a8a9a24b89
risvh <67486979+risvh@users.noreply.github.com>
2023-08-14 01:38:52 +0800

    Discord related changes to makeFullGameFolderForRelease.sh

--- repo/makeFullGameFolderForRelease.sh
+++ repo/makeFullGameFolderForRelease.sh
@@ -9,8 +9,13 @@ fi
 cd "$(dirname "${0}")/../.."
 
 
-###### Name the release folder
+###### Paths Required by Discord SDK
+COMPILE_ROOT=$(pwd)
+DISCORD_SDK_PATH="$COMPILE_ROOT/dependencies/discord_game_sdk"
+MINOR_GEMS_PATH="$COMPILE_ROOT/minorGems"
 
+
+###### Name the Release Folder
 version=$(<OneLifeData7/dataVersionNumber.txt)
 if [[ $PLATFORM == 1 ]]; then outputName="2HOL_v${version}_linux"; fi
 if [[ $PLATFORM == 5 ]]; then outputName="2HOL_v${version}_win"; fi
@@ -19,8 +24,7 @@ mkdir -p $outputName
 cd $outputName
 
 
-###### Asset folders and other files
-
+###### Copy Asset folders
 pathList=()
 folderList=()
 
@@ -31,7 +35,6 @@ for f in $temp; do pathList+=( "../OneLifeData7/$f" ); done;
 temp="graphics otherSounds languages settings"
 for f in $temp; do folderList+=( "$f" ); done;
 for f in $temp; do pathList+=( "../OneLife/gameSource/$f" ); done;
-
 
 for i in ${!pathList[@]}; do
     
@@ -54,6 +57,7 @@ for i in ${!pathList[@]}; do
 done;
 
 
+###### Copy Data Version and other files
 cp ../OneLife/gameSource/language.txt .
 cp ../OneLife/gameSource/us_english_60.txt .
 cp ../OneLife/documentation/Readme.txt .
@@ -64,20 +68,15 @@ cp ../OneLife/server/wordList.txt .
 d=$(TZ=":GMT" date)
 echo "$1 built on $d" > binary.txt
 
-
-###### Data version
-
 cp ../OneLifeData7/dataVersionNumber.txt .
 
 
 ###### SDL and clearCache script
-
 if [[ $PLATFORM == 5 ]] && [ ! -f SDL.dll ]; then cp ../OneLife/build/win32/SDL.dll .; fi
 if [[ $PLATFORM == 5 ]] && [ ! -f clearCache.bat ]; then cp ../OneLife/build/win32/clearCache.bat .; fi
 
 
-###### EOL
-
+###### Change EOL
 if [[ $PLATFORM == 1 ]]; then
     find . -type f -name '*.txt' -exec sed -i 's/\r//g' {} +
 fi
@@ -101,22 +100,40 @@ if [[ $PLATFORM == 5 ]]; then
 fi
 
 
-###### Compile the game exe
-
+###### Compile the Game Exe
 cd ../miniOneLifeCompile
 ./cleanOldBuildsAndOptionallyCaches.sh 1
 ./applyFixesAndOverride.sh
 cd ..
 
 cd OneLife
-./configure $PLATFORM
-
+if [ -d $DISCORD_SDK_PATH ]; then
+	./configure $PLATFORM "$MINOR_GEMS_PATH" --discord_sdk_path "${DISCORD_SDK_PATH}"
+else
+	./configure $PLATFORM
+fi
 cd gameSource
 if [[ $PLATFORM == 5 ]]; then export PATH="/usr/i686-w64-mingw32/bin:${PATH}"; fi
 make
 cd ../..
 cd $outputName
 
+
+###### Copy Discord SDK
+if [ -d $DISCORD_SDK_PATH ]; then
+	# windows: copy discord_game_sdk.dll into the output folder
+	if [[ $PLATFORM == 5 ]]; then cp $DISCORD_SDK_PATH/lib/x86/discord_game_sdk.dll ./; fi
+	# linux: copy discord_game_sdk.so into the output folder
+	if [[ $PLATFORM == 1 ]]; then
+		if [[ ! -f ./discord_game_sdk.so ]]; then
+			sudo cp $DISCORD_SDK_PATH/lib/x86_64/discord_game_sdk.so ./
+			sudo chmod a+r ./discord_game_sdk.so
+		fi
+	fi
+fi
+
+
+###### Copy to Game Folder
 if [[ $PLATFORM == 1 ]]; then
     mv -f ../OneLife/gameSource/OneLife .
 fi
